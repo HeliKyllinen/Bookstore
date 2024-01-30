fd617c3cabd706782883b63d8a2c21c0025b84da
Heli Kyllinen <helikyllinen@gmail.com>
2024-01-30 21:42:07 +0200

    first commit

---
+++ src/main/java/K24/Bookstore/web/BookController.java
@@ -0,0 +1,15 @@
+package K24.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BookController {
+	
+	@GetMapping("index")
+	@ResponseBody
+	public String returnMessage() {
+		return "This is the main page";
+	}
+}
