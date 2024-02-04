1b73000e25bbe17666e2e780b3fa5587d8ec1e15
Heli Kyllinen <helikyllinen@gmail.com>
2024-02-04 12:04:21 +0200

    Lisätty 3.viikon 3.tehtävä ja aloitettu 4.tehtävää

--- src/main/java/K24/Bookstore/web/BookController.java
+++ src/main/java/K24/Bookstore/web/BookController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import K24.Bookstore.domain.Book;
 import K24.Bookstore.domain.BookRepository;
 
 @Controller
@@ -13,11 +17,34 @@ public class BookController {
 	@Autowired
 	private BookRepository bookRepository;
 	
-
-	
 	@GetMapping("/booklist")
 	public String showBooks(Model model) {
 		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";
 	}
+	
+	@GetMapping("/addbook")
+	public String addBook(Model model) {
+		model.addAttribute("book", new Book() );
+		return "addbook";
+	}
+	
+	@PostMapping("/saveBook")
+	public String saveBook(@ModelAttribute("book") Book book) {
+		bookRepository.save(book);
+		return "redirect:/booklist";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long id, Model model) {
+		bookRepository.deleteById(id);
+		return "redirect:/booklist"; 
+	}
+	
+	@GetMapping("/editBook/{id}")
+	public String editBook(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("editBook", bookRepository.findById(id));
+		return "editBook"; 
+	}
+	
 }
