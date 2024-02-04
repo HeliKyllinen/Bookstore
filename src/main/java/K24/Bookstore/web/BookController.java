d92716f4c47d5e8adf78b82e261f558ac4d445f9
Heli Kyllinen <helikyllinen@gmail.com>
2024-02-04 10:26:47 +0200

    Lisätty 3. viikon 2. tehtävä

--- src/main/java/K24/Bookstore/web/BookController.java
+++ src/main/java/K24/Bookstore/web/BookController.java
@@ -4,25 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import K24.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+	
 	@Autowired
-	private BookRepository repository;
+	private BookRepository bookRepository;
 	
-	@RequestMapping(value= {"/", "/booklist"})
-	public String bookList(Model model) {
-		model.addAttribute("books", repository.findAll());
+
+	
+	@GetMapping("/booklist")
+	public String showBooks(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";
-	}
-	
-	@GetMapping("index")
-	@ResponseBody
-	public String returnMessage() {
-		return "This is the main page";
 	}
 }
