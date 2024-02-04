38315fa2eca8538c6da76b613aeda738f7b8ced1
Heli Kyllinen <helikyllinen@gmail.com>
2024-02-04 12:42:21 +0200

    3.viikon 4 ja 5 tehtävä tehty

--- src/main/java/K24/Bookstore/BookstoreApplication.java
+++ src/main/java/K24/Bookstore/BookstoreApplication.java
@@ -1,8 +1,6 @@
 package K24.Bookstore;
 
 import org.springframework.boot.CommandLineRunner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
